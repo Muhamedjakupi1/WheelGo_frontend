b118e8e18f4458bbdf647d2a7aacf4da77ee3fac
dua zogu <duazogu@gmail.com>
2026-04-22 23:36:54 +0200

    SCRUM-18 changes for url login page

--- my-app/src/api/authApi.js
+++ my-app/src/api/authApi.js
@@ -4,8 +4,8 @@ const pub = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
 });
 
-export const login = (email, password) =>
-  pub.post("/api/auth/login", { email, password });
+export const login = (tenantSlug, email, password) =>
+  pub.post(`/api/auth/login/${tenantSlug}`, { email, password });
 
 export const signupTenant = (tenantSlug, email, password) =>
   pub.post(`/api/auth/signup/${tenantSlug}`, { email, password });
